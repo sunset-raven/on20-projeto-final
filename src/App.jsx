6750e73cdb389d0443b4b84a60821d4292809da1
Andrea Vetter <andreavetterda@gmail.com>
2022-12-17 16:27:02 -0300

    What is happening here

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <div className="App">
-    <BrowserRouter>
-      <Header />
-      <Title />
-      <ApplicationRoutes />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Title />
+        <ApplicationRoutes />
+        <Footer />
       </BrowserRouter>
     </div>
   )
--- src/pages/Busca.jsx
+++ src/pages/Busca.jsx
@@ -1,26 +1,60 @@
 import { useState, useEffect } from "react"
 import Axios from 'axios'
+import styles from '../styles/pages/busca.module.css'
 
 
 export function Busca() {
+    const inputBusca = document.getElementById('inputBusca')
 
     const [result, setResult] = useState([])
     const [searchedTerm, setSearchedTerm] = useState('')
-    const [filteredRepos, setfilteredRepos] = useState([])
-    const baseURL = '/api/users/random_user/v2/users'
+    const [filteredResults, setFilteredResults] = useState([])
 
     useEffect(() => {
         async function getData() {
-            const response = await Axios.get('https://random-data-api.com/api/v2/users/{')
-            setResult(response.data)
+            const response = await Axios.get('https://random-data-api.com/api/v2/users?size=10&is_json=true')
+            const final = response.data
+            setResult(final)
+            console.log(final)
         }
         getData()
     }, [])
 
+    function handleSearch(event) {
+        setSearchedTerm(event.target.value)
+    }
+
+    useEffect(() => {
+        const filtered = result.filter(professional => {
+            return professional.first_name.includes(searchedTerm)
+        })
+        setFilteredResults(filtered)
+    }, [result, searchedTerm])
+
     return (
         <>
-        <input type="text" />
-        <p>{result}</p>
+            <div className={styles.searchBegin}>
+                <p>Insira o primeiro nome da profissional para iniciar a busca:</p>
+                <input type="text" className={styles.inputBusca} id="inputBusca" onChange={handleSearch} />
+            </div>
+            {filteredResults.map(professionals => {
+                return (
+                    inputBusca.value !== "" &&
+                    <div key={professionals.id} id=''>
+                        <div className={styles.searchInfo}>
+                            <div>
+                                <div className={styles.firstAndLastName}>
+                                    <p>{professionals.first_name} </p>
+                                    <p> {professionals.last_name}</p>
+                                </div>
+                                <p className={styles.email}>{professionals.email}</p>
+                                <p className={styles.gender}>{professionals.gender}</p>
+                            </div>
+                            <img className={styles.Image} src={professionals.avatar} width="70" height="70" />
+                        </div>
+                    </div>
+                )
+            })}
         </>
     )
 }
