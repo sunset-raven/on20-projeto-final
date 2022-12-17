62b69e06ce53125b66f45fc7c9220f05394c86f9
Andrea Vetter <andreavetterda@gmail.com>
2022-12-16 22:46:40 -0300

    5th commit - trying to change href to path.

--- src/pages/Busca.jsx
+++ src/pages/Busca.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react"
+import Axios from 'axios'
+
+
 export function Busca() {
+
+    const [result, setResult] = useState([])
+    const [searchedTerm, setSearchedTerm] = useState('')
+    const [filteredRepos, setfilteredRepos] = useState([])
+    const baseURL = '/api/users/random_user/v2/users'
+
+    useEffect(() => {
+        async function getData() {
+            const response = await Axios.get('https://random-data-api.com/api/v2/users/{')
+            setResult(response.data)
+        }
+        getData()
+    }, [])
 
     return (
         <>
+        <input type="text" />
+        <p>{result}</p>
         </>
     )
 }
